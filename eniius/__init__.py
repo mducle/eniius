ca820ada9d38861427d84364e2bdf819100d8c48
Duc Le <duc.le@stfc.ac.uk>
2023-05-12 14:18:47 +0100

    Add CI for release (#4)

--- eniius/__init__.py
+++ eniius/__init__.py
@@ -1,7 +1,14 @@
-from . import mcstas
-from . import horace
-from . import nexus
-from .eniius import Eniius
+import warnings
+try:
+    from . import mcstas
+    from . import horace
+    from . import nexus
+    from .eniius import Eniius
+except ModuleNotFoundError as e:
+    import traceback
+    warnings.warn('Could not import submodule')
+    traceback.print_tb(e.__traceback__)
+    print(e)
 
 from . import _version
 __version__ = _version.get_versions()['version']
