4008a8ee635c2e49f82663a2a4ae2f3960ca89c6
Duc Le <duc.le@stfc.ac.uk>
2023-06-03 12:53:30 +0100

    Bugfixes in initial use
    Fix wrong secret and wheel path in gh-action
    Update horace.py to handle chopper speeds
    Fix bug in icp writer to handle single-xtal detector.dat

--- scripts/mcstas_nxs_inst.py
+++ scripts/mcstas_nxs_inst.py
@@ -5,7 +5,7 @@ import eniius
 
 
 def mcstas2nxs(instrfile):
-    detfile = os.path.join(os.path.dirname(eniius.__file__), 'detector.dat')
+    detfile = os.path.join(os.path.dirname(eniius.__file__), 'instruments', 'detector.dat')
     wrapper = eniius.Eniius.from_mcstas(instrfile, detfile)
     wrapper.to_json(f'mcstas_{wrapper.name}.json')
     wrapper.to_icp(f'mcstas_{wrapper.name}.nxs')
