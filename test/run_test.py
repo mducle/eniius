af636027f422e08688d16b68ce51b7968b469670
Gregory Tucker <gregory.tucker@gmail.com>
2023-07-05 08:20:47 +0200

    [Fix] Empty-parameter [Add] test for output empty parameter

--- test/run_test.py
+++ test/run_test.py
@@ -5,6 +5,7 @@ import tempfile
 import os
 import nexusformat.nexus as nexus
 import eniius
+import json
 
 class EniiusTest(unittest.TestCase):
 
@@ -69,6 +70,57 @@ class EniiusTest(unittest.TestCase):
             self.assertTrue('fermi' in root['instrument'])
             self.assertEqual(root['instrument/fermi/energy'].nxvalue, Ei)
             self.assertTrue('sample' in root)
+
+    def test_save_json_from_mcstas(self):
+        jsonfile = os.path.join(self.tmpdir.name, 'mcstas.json')
+        instrfile = os.path.join(self.rootdir, 'instruments', 'isis_merlin.instr')
+        wrapper = eniius.Eniius.from_mcstas(instrfile, self.detdat)
+        wrapper.to_json(jsonfile)
+        with open(jsonfile) as file:
+            data = json.load(file)
+
+        nx_class_fields = ('name', 'type', 'children', 'attributes')
+
+        self.assertTrue(len(data) == 1)
+        self.assertTrue('children' in data)
+        children = data['children']
+        self.assertTrue(len(children) == 1)
+
+        root = children[0]
+        for field in nx_class_fields:
+            self.assertTrue(field in root)
+        self.assertTrue('root' == root['name'])
+        self.assertTrue(len(root['children']) == 1)
+        self.assertTrue(len(root['attributes']) == 1)
+        self.assertTrue(root['attributes'][0]['values'] == 'NXentry')
+
+        instrument = root['children'][0]
+        for field in nx_class_fields:
+            self.assertTrue(field in instrument)
+        self.assertTrue(len(instrument['children']) == 14)
+        self.assertTrue(len(instrument['attributes']) == 1)
+        self.assertTrue(instrument['attributes'][0]['values'] == 'NXinstrument')
+
+        named_children = [x for x in instrument['children'] if 'name' in x]
+        self.assertTrue(len(named_children) == 13)
+        self.assertTrue('mcstas' in [x['name'] for x in named_children])
+        mcstas_children = [x for x in named_children if x['name'] == 'mcstas']
+        self.assertTrue(len(mcstas_children) == 1)
+
+        for field in nx_class_fields:
+            self.assertTrue(field in mcstas_children[0])
+        named_mcstas_children = [x for x in mcstas_children[0]['children'] if 'name' in x]
+        self.assertTrue('declare' in [x['name'] for x in named_mcstas_children])
+        declare = [x for x in named_mcstas_children if x['name'] == 'declare']
+        self.assertTrue(len(declare) == 1)
+
+        for field in nx_class_fields:
+            self.assertTrue(field in declare[0])
+        parameters = declare[0]['children']
+        for name in ('slit_curv', 'num_slits', 'width', 'len', 'phase_time', 'E_min', 'E_max'):
+            self.assertTrue(len([x for x in parameters if x['config']['name'] == name]) == 1)
+        # Check that extraneous empty parameters do not get saved:
+        self.assertTrue(len([x for x in parameters if x['config']['name'] == '']) == 0)
 
 
 if __name__ == '__main__':
